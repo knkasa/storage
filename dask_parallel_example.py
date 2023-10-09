6bafbd0df62774dc6319ac9a2522e297cad5b4d5
Ken Nakatsukasa <knkasa@gmail.com>
2023-10-10 00:34:54 +0900

    Delete dask_parallel_example.py

--- dask_parallel_example.py
+++
@@ -1,8 +0,0 @@
-
-import dask.dataframe as dd
-exit()
-
-df = dd.read_csv('your_large_file.csv')
-
-# Perform operations on df like you would with a Pandas DataFrame
-result = df.groupby('column_name').mean().compute()
