24d72bf9acbce70eb1f8df484bde5cba2d29473b
Ken Nakatsukasa <knkasa@gmail.com>
2023-10-28 22:36:38 +0900

    Add files via upload

--- docker_test/test.py
+++ docker_test/test.py
@@ -1,4 +1,9 @@
 import os
+import pandas as pd
+import numpy as np
+
 print("testing docker on local")
 print(os.environ.get('MY_STRING'))
 print(os.getenv("MY_ENV_VAR"))
+df = pd.DataFrame({'col':np.random.rand(10)})
+df.to_csv('/my_dir/res.csv')
