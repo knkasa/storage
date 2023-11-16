95a7e15a0803f30c73f7a9ce97955be17da80bc4
Ken Nakatsukasa <knkasa@gmail.com>
2023-11-17 01:05:03 +0900

    Add files via upload

--- categorical_correlation.py
+++ categorical_correlation.py
@@ -10,3 +10,25 @@ def cramers_v(x, y):
     rcorr = r-((r-1)**2)/(n-1)
     kcorr = k-((k-1)**2)/(n-1)
     return np.sqrt(phi2corr/min((kcorr-1),(rcorr-1)))
+    
+
+#The tetrachoric correlation coefficient is a more accurate measure of the association between two binary variables than the phi coefficient, but it is also more computationally expensive.
+#Here is an example of how to calculate the phi coefficient and the tetrachoric correlation coefficient between two binary variables in Python:
+#The polychoric correlation coefficient is used when both variables are categorical with more than two labels.
+import pandas as pd
+from scipy.stats import chi2_contingency, tetrachoric
+
+# Create a DataFrame
+df = pd.DataFrame({'X': [0, 1, 1, 0, 1, 0, 1, 0], 'Y': [1, 0, 1, 1, 0, 1, 0, 1]})
+
+# Calculate the phi coefficient
+contingency_table = pd.crosstab(df['X'], df['Y'])
+chi2, pval, deg_freedom, expected_counts = chi2_contingency(contingency_table)
+phi = np.sqrt(chi2 / df.shape[0])
+
+# Calculate the tetrachoric correlation coefficient
+tetrachoric_corr = tetrachoric(contingency_table)
+
+# Print the results
+print("Phi coefficient:", phi)
+print("Tetrachoric correlation coefficient:", tetrachoric_corr)
