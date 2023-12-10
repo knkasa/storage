7234db6987fd8813b53efd4a93ecbc9ae4318678
Ken Nakatsukasa <knkasa@gmail.com>
2023-12-10 16:37:45 +0900

    Add files via upload

---
+++ loguru_example.py
@@ -0,0 +1,25 @@
+import pdb
+import os
+from loguru import logger
+import time
+
+os.chdir('C:/Users/knkas/Desktop/NLP_example')
+
+logger.add(
+    "example.log", 
+    rotation="100MB", 
+    level="DEBUG", 
+    
+    # {level: <8} means put 8 spaces after the level text.  
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | {name} | {message}", 
+    )
+
+t = time.perf_counter()
+
+logger.debug(f"This is a debug message at {t}.")
+logger.info("This is an info message")
+logger.warning("This is a warning message")
+logger.error("This is an error message")
+logger.critical("This is a critical message")
+
+
