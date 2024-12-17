a888c1ba4ba36cd735df98107b475de2ba4e0735
Ken Nakatsukasa <knkasa@gmail.com>
2024-12-17 16:20:20 +0900

    Update joblib_example.py

--- parallel/joblib_example.py
+++ parallel/joblib_example.py
@@ -6,7 +6,7 @@ def func(x, a):
 a = 2
 x_list = [1, 2, 3]
 
-# Using threading backend
+# Choose "threading" or "multiprocessing".
 results = Parallel(n_jobs=2, backend="threading")(
     delayed(func)(x, a) for x in x_list
 )
