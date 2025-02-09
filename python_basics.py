fd67401adc424e45b533db2af5d3631088a9a2bc
Ken Nakatsukasa <knkasa@gmail.com>
2025-02-09 21:08:35 +0900

    Update python_basics.py

--- python_basics.py
+++ python_basics.py
@@ -239,55 +239,6 @@ dt.utcnow()
 #--------------------------------------------------------------
 
 
-#------------- matplotlib -------------------------------------
-import japanize_matplotlib  # 日本語表記のmaplotlib
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
-figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')  # need to import "figure" module. 
-plt.plot( x,y , 'bo', label='line1'  )   # need to put labels for legend 
-plt.plot( x2, y2, 'k-', label='line2' )
-plt.legend( loc=('upper right'), bbox_to_anchor=(1.2, 0.5) ); # use either loc or bbox to set legend position.
-plt.xlabel('x'); plt.ylabel('y') 
-plt.xticks(rotation=45);  plt.xticks([]);   #remove x-axis  
-plt.xticks(np.arange(0, len(x)+1, 5))  ;   # show only xlabel every 5 points
-plt.tight_layout()  # fit graph size to window
-plt.xlim(x_min, x_max)  # define axis range  
-
-plt.show() 
-plt.savefig('../image.png' ,  dpi=400 )
-plt.clf()   # this clears plot.  (useful when using loop)
-
-plt.imshow(Ximage[:,:], extent=[0,1,0,1] )   # Image plot,  Ximage is numpy array.  extent is xy-axis range
-plt.xlabel('x axis')
-plt.colorbar(); plt.grid(False)
-plt.show()
-
-ax = plt.gca()
-data.plot( x='col1', y='val1', ax=ax )   # pandas dataframe
-data.plot( x='col2', y='val2', ax=ax )
-plt.show()
-
-plt.subplot(4,2,1)   # subplot, (4 rows, 2 columns, plot index)
-plt.plot(x,y)
-plt.subplot(4,2,2)   
-plt.plot(x,y)
-plt.show()
-
-# histogram
-plt.hist( diff_dist, bins=np.arange(1,1000,100), density=False, edgecolor='black' )  # bin size [1,100), [100,200), ...
-plt.hist( diff_dist, bins=10, density=False )  # bins=10 means there will be 10 bins  
-
-# figure number.  # set figure_number to muti-plot in same figure. 
-figure(num=figure_number, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')  
-plt.plot(x,y)
-
-plt.imshow( pnl_mat_interp, extent=[0,1,0,1] )  # extent defines x,y axis domain range
-
-# If you need to show japanese characters, use
-#https://albertauyeung.github.io/2020/03/15/matplotlib-cjk-fonts.html/
-
-#-------------------------------------------------------------
-
 #--------- numpy ----------------------------------------------
 x = np.arange(2,10,2)
 np.random.rand()
