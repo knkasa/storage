516dfaf0833465ed80fc6b36e7db687338a46fd8
Ken Nakatsukasa <knkasa@gmail.com>
2025-04-07 18:52:27 +0900

    Add files via upload

---
+++ package_python/__init__.py
@@ -0,0 +1,3 @@
+# your_package_name/__init__.py
+from .module1 import your_function1
+from .module2 import your_function2
---
+++ package_python/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your-package-name',              # Package name
+    version='0.1.0',                       # Version
+    author='Your Name',                    # Author name
+    author_email='your@email.com',         # Contact
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/your-repo',  # Optional
+    packages=find_packages(),              # Automatically find all packages
+    install_requires=[                     # Dependencies
+        'numpy>=1.20',
+        'pandas',
+    ],
+    classifiers=[                          # Optional Metadata
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
