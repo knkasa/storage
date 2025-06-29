11b54ae362b667dc586146ab0a653f3023e67a15
Ken Nakatsukasa <knkasa@gmail.com>
2025-06-29 21:16:40 +0900

    Create plotly_sunburst.py

---
+++ plot/plotly_sunburst.py
@@ -0,0 +1,4 @@
+fig = px.sunburst(df1, path = ['Country', 'Genre'], values = 'Count', color = 'Country',
+                 color_discrete_map = {'united states': '#85e0e0', 'india': '#99bbff', 'united kingdom': '#bfff80'})
+fig.update_layout(title_text='Distribution of Genres in India, US, UK', title_x=0.5)                  
+fig.show()
