3ab2c92f749f8b2a66d6d0531b4bfb41dfc6beb9
Ken Nakatsukasa <knkasa@gmail.com>
2025-08-07 11:12:20 +0900

    Update plotly_sunburst.py

--- plot/plotly_sunburst.py
+++ plot/plotly_sunburst.py
@@ -1,5 +1,6 @@
 fig = px.sunburst(df1, path = ['Country', 'Genre'], values = 'Count', color = 'Country',
                  color_discrete_map = {'united states': '#85e0e0', 'india': '#99bbff', 'united kingdom': '#bfff80'})
 fig = px.sunburst( df, path=['colA', 'colB'], value='colC')
-fig.update_layout(title_text='Distribution of Genres in India, US, UK', title_x=0.5)                  
+fig.update_layout(title_text='Distribution of Genres in India, US, UK', title_x=0.5)  
+fig.update_traces(hovertemplate="%{label}: %{value} <br>Percent1: %{percentParent:.2f} <br>Percent2: %{percentRoot:.2%}")
 fig.show()
