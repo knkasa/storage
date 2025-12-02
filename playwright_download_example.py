e770e63c59618d19d0d88d81a6ded6d4a1d043a1
Ken Nakatsukasa <knkasa@gmail.com>
2025-12-02 15:32:46 +0900

    Update playwright_download_example.py

--- playwright_download_example.py
+++ playwright_download_example.py
@@ -1,9 +1,18 @@
-    with page.expect_download() as download_info:
-        target.click(timeout=30000, no_wait_after=True)
-    download = download_info.value
+# downloading.
+with page.expect_download() as download_info:
+    target.click(timeout=30000, no_wait_after=True)
+download = download_info.value
 
-    # Persist the file to a known path
-    suggested_name = download.suggested_filename
-    final_path = r"C:\Users\中務健\Desktop\{}".format(suggested_name)
-    download.save_as(final_path)
-    print(f"Downloaded -> {final_path}")
+# Persist the file to a known path
+suggested_name = download.suggested_filename
+final_path = r"C:\Users\中務健\Desktop\{}".format(suggested_name)
+download.save_as(final_path)
+print(f"Downloaded -> {final_path}")
+
+# Typing texts in search box.  Pressing Tab helps focus the search box.
+page.get_by_role("checkbox").nth(1).click()  #open search box
+page.wait_for_timeout(500)
+page.keyboard.press("Tab")  #press Tab
+page.wait_for_timeout(200)
+page.keyboard.type("神戸_20251127_テスト", delay=50) #Type texts
+
